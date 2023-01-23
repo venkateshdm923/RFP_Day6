0b91cee480798b4c6c32e483813f554c2b677521
VENKATESH MARADDI <venkateshdm923@gmail.com>
2023-01-23 23:08:09 +0530

    Char to string

---
+++ CharToString.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.fourthpackage;
+
+public class CharToString {
+
+	
+	 public static void main(String args[])
+    {
+		 
+    
+ 
+        // Declaring and initializing a character array
+        char s[] = { 'B', 'r', 'i', 'd', 'g', 'e', 'l', 'a',
+                     'b', 'z'};
+ for(int i=0;i<s.length;i++) {
+        // Printing converted string from character array
+        System.out.print(s[i]);
+    }
+}}
+
